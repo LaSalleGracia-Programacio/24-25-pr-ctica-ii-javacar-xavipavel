9459a6bdf021edc2454ebc873f3d52ae155d5512
XavierFarrus <xavier.farrus@gracia.lasalle.cat>
2025-03-26 15:29:33 +0100

    Update EtiquetaAmbiental.java

--- src/main/java/org/JavaCar/EtiquetaAmbiental.java
+++ src/main/java/org/JavaCar/EtiquetaAmbiental.java
@@ -1,4 +1,24 @@
 package org.JavaCar;
 
 public enum EtiquetaAmbiental {
+    ETIQUETA_0_EMISIONES("Etiqueta 0 Emisiones", "Azul"),
+    ETIQUETA_ECO("Etiqueta Eco", "Verde"),
+    ETIQUETA_C("Etiqueta C", "Verde"),
+    ETIQUETA_B("Etiqueta B", "Amarilla");
+
+    private String nombre;
+    private String color;
+
+    EtiquetaAmbiental(String nombre, String color) {
+        this.nombre = nombre;
+        this.color = color;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getColor() {
+        return color;
+    }
 }
