a4be6804fa8989888109a60084f468636b4eea2b
XavierFarrus <xavier.farrus@gracia.lasalle.cat>
2025-03-26 17:04:02 +0100

    Correccion de errores

--- src/main/java/org/JavaCar/Roda.java
+++ src/main/java/org/JavaCar/Roda.java
@@ -4,15 +4,16 @@ public class Roda {
     private String marca;
     private double diametre;
 
-    public Roda(String Marca, double diametre){
-        this.marca = Marca;
+    public Roda(String marca, double diametre) {
+        this.marca = marca;
         this.diametre = diametre;
     }
 
-    public String getMarca(){
+    public String getMarca() {
         return marca;
     }
-    public double getDiametre(){
+
+    public double getDiametre() {
         return diametre;
     }
 }
